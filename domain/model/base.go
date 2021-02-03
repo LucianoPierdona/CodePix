bc6553ac727c3f5e71d1c5982d4b9862fc0d030b
LucianoPierdona <lucianompj1@gmail.com>
2021-02-02 22:59:26 -0300

    created main and pix files

--- domain/model/base.go
+++ domain/model/base.go
@@ -6,11 +6,11 @@ import (
 )
 
 func init() {
-	goValidator().SetFieldsRequiredByDefault(true)
+	govalidator.SetFieldsRequiredByDefault(true)
 }
 
 type Base struct {
-	ID string `json:"id" valid:"uuid"`
+	ID        string    `json:"id" gorm:"type:uuid;primary_key" valid:"uuid"`
 	CreatedAt time.Time `json:"created_at" valid:"-"`
 	UpdatedAt time.Time `json:"updated_at" valid:"-"`
 }
