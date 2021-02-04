db93eee385b6fae1b5387a8ccab97a742eba7f40
LucianoPierdona <lucianompj1@gmail.com>
2021-02-03 23:07:52 -0300

    created process file

--- cmd/kafka.go
+++ cmd/kafka.go
@@ -25,14 +25,12 @@ import (
 var kafkaCmd = &cobra.Command{
 	Use:   "kafka",
 	Short: "Start consuming transactions using Apache Kafka",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kafka called")
+		fmt.Println("creating message")
+		deliveryChan := make(chan ckafka.Event)
+		producer := kafka.NewKafkaProducer()
+		kafka.Publish("Hey Kafka", "test", producer, deliveryChan)
+		kafka.DeliveryReport(deliveryChan)
 	},
 }
 
