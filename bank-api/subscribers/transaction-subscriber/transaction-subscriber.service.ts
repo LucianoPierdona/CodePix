abcdaba9ee468205323979ed76a393fe44457a92
LucianoPierdona <lucianompj1@gmail.com>
2021-02-06 15:24:17 -0300

    created transaction-subscriber

---
+++ bank-api/subscribers/transaction-subscriber/transaction-subscriber.service.ts
@@ -0,0 +1,35 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { BankAccount } from '../../models/bank-account.model';
+import { Transaction } from '../../models/transaction.model';
+import {
+  Connection,
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+  Repository,
+} from 'typeorm';
+
+@EventSubscriber()
+export class TransactionSubscriber
+  implements EntitySubscriberInterface<Transaction> {
+  constructor(
+    connection: Connection,
+    @InjectRepository(BankAccount)
+    private bankAccountRepo: Repository<BankAccount>,
+  ) {
+    connection.subscribers.push(this);
+  }
+
+  listenTo() {
+    return Transaction;
+  }
+
+  async afterInsert(event: InsertEvent<Transaction>) {
+    const transaction = event.entity;
+    const bankAccount = await this.bankAccountRepo.findOneOrFail(
+      transaction.bank_account_id,
+    );
+    bankAccount.balance = bankAccount.balance + transaction.amount;
+    await this.bankAccountRepo.save(bankAccount);
+  }
+}
